a9cca733ab7d78baef105feeb08fbdf5e18af366
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-10-24 20:22:56 -0300

    Exemplos basicos de (FileStream/StreamReader)V2-Forma Aprimorada

--- FilestreamStreamreader/FilestreamStreamreader/Program.cs
+++ FilestreamStreamreader/FilestreamStreamreader/Program.cs
@@ -7,15 +7,16 @@ namespace FilestreamStreamreader
         static void Main(string[] args)
         {
             string path = @"c:\temp\file1.txt";
-            FileStream fs = null;
             StreamReader sr = null;
 
             try
             {
-                fs = new FileStream(path, FileMode.Open);
-                sr = new StreamReader(fs);
-                string line = sr.ReadLine();
-                Console.WriteLine(line);
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
             }
             catch (IOException e)
             {
@@ -25,7 +26,6 @@ namespace FilestreamStreamreader
             finally
             {
                 if(sr != null) sr.Close();
-                if(fs != null) fs.Close();
             }
         }
     }
