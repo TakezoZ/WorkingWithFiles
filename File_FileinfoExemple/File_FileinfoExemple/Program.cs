5679f66e4ba61365091f22c6c61df94755fed269
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-10-24 19:49:09 -0300

    Exemplos basicos de (File/FileInfo/IOException)

---
+++ File_FileinfoExemple/File_FileinfoExemple/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace File_FileinfoExemple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sorcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sorcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(targetPath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
