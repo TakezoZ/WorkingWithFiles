635530825e0960f545bf83d584a2f7b0bc59a573
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-10-28 21:57:41 -0300

    Exemplo StreamWriter

---
+++ StreamWriter/StreamWriter/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace StreamWriter_Exemple
+{
+    class Program
+    {
+        static void Main(string[] Args)
+        {
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
