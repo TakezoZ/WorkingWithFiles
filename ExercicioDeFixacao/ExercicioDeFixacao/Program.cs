3c01d64de63d4afc42a68cf99607685d0fc8d8d9
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-10-29 00:27:20 -0300

    Exercicio de Fixação-V2(Minha correção/Otimização)

--- ExercicioDeFixacao/ExercicioDeFixacao/Program.cs
+++ ExercicioDeFixacao/ExercicioDeFixacao/Program.cs
@@ -1,5 +1,4 @@
 ﻿using ExercicioDeFixacao.Entities;
-using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 
 namespace Exemple_Path
@@ -11,34 +10,36 @@ namespace Exemple_Path
             Console.WriteLine("Enter file full path: ");
             string sourceFile = Console.ReadLine();
 
-            List<Product> produtos = new List<Product>();
             try
             {
                 string sourceFolderPath = Path.GetDirectoryName(sourceFile);
                 string targetFolderPath = @$"{sourceFolderPath}\out";
                 string targetFile = @$"{targetFolderPath}\summary.csv";
 
-                string[] lines = File.ReadAllLines(sourceFile);
-
                 Directory.CreateDirectory(targetFolderPath);
 
-                foreach (string line in lines)
-                {
-                    string[] fields = line.Split(',');
-                    string name = fields[0];
-                    double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
-                    int amount = int.Parse(fields[2]);
-                    produtos.Add(new Product(name, price, amount));
-                }
                 using (StreamWriter summary = File.AppendText(targetFile))
                 {
-                    foreach (Product produto in produtos)
+                    string[] lines = File.ReadAllLines(sourceFile);
+
+                    foreach (string line in lines)
                     {
-                        summary.WriteLine($"{produto.Name},{produto.Total().ToString("F2",CultureInfo.InvariantCulture)}");
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int amount = int.Parse(fields[2]);
+                        Product produto = new Product(name, price, amount);
+
+                        summary.WriteLine($"{produto.Name},{produto.Total().ToString("F2", CultureInfo.InvariantCulture)}");
                     }
                 }
             }
             catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            catch(SystemException e)
             {
                 Console.WriteLine("An error occurred");
                 Console.WriteLine(e.Message);
