d1e4089ef42a8aa619282f9fe80057e40f4c3f17
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-10-28 21:48:42 -0300

    Exemplo Bloco Using-V2(Estanciação resmida)

--- UsingBlock/UsingBlock/Program.cs
+++ UsingBlock/UsingBlock/Program.cs
@@ -4,20 +4,20 @@
     {
         static void Main(string[] Args)
         {
-            string path = @"c:\temp\filex.txt";
+            string path = @"c:\temp\file1.txt";
 
             try
             {
-                using (FileStream fs = new FileStream(path, FileMode.Open))
+                using (StreamReader sr = File.OpenText(path))
                 {
-                    using (StreamReader sr = new StreamReader(fs))
+                    while (!sr.EndOfStream)
                     {
                         string line = sr.ReadLine();
                         Console.WriteLine(line);
                     }
                 }
             }
-            catch(IOException e)
+            catch (IOException e)
             {
                 Console.WriteLine("An error occurred");
                 Console.WriteLine(e.Message);
