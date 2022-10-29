b9a479b30f98c36e6e642594cb28f6fd10988a8a
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-10-28 21:45:18 -0300

    Exemplo Bloco Using-V1

---
+++ UsingBlock/UsingBlock/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace UsingBlock
+{
+    class Program
+    {
+        static void Main(string[] Args)
+        {
+            string path = @"c:\temp\filex.txt";
+
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
